1946495665c508bd37d1e6db6f5fa1dee464dc91
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-02-06 23:50:12 -0500

    Subset the data

--- load.r
+++ load.r
@@ -1,5 +1,43 @@
-# Read the historical history dissertations data; hd = historical dissertations
-hd <- read.csv("data/history-df.csv", stringsAsFactors=F)
+# Read the historical history dissertations data
+import <- read.csv("data/history-df.csv", stringsAsFactors=F)
 
 # Turn empty strings into NAs
-hd[hd == ""] <- NA
+import[import == ""] <- NA
+
+# Figure out the top subjects
+subject_count <- summarise(group_by(import, subject1), count = length(id)) 
+subject_count <- arrange(subject_count, count)
+
+# Limit the data frame to only historical subjects
+historical_subjects <- c("History, Modern.",
+                         "History, United States.",
+                         "History, European.",
+                         "Religion, History of.",
+                         "Biography.",
+                         "History, Asia, Australia and Oceania.",
+                         "Art History.",
+                         "History, Canadian.",
+                         "Education, History of.",
+                         "History, General.",
+                         "History, Latin American.",
+                         "History, Black.",
+                         "History, African.",
+                         "Economics, History.",
+                         "History, Middle Eastern.",
+                         "History, Medieval.",
+                         "History, Ancient.",
+                         "History of Science.",
+                         "History, Church.",
+                         "History, Russian and Soviet.",
+                         "History, World History.",
+                         "History, Military.",
+                         "History, History of Oceania.")
+historical <- filter(import, match(subject1, historical_subjects))
+
+# Count the number of each degree
+degree_count <- summarise(group_by(import, degree), count = length(id)) 
+degree_count <- arrange(degree_count, count)
+
+# Having limited the data frame to historical work, let's limit it to PhDs and MAs
+h_ma   <- filter(historical, degree == "M.A.")
+h_diss <- filter(historical, degree == "Ph.D.")
