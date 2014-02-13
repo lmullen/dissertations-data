4ba0312a6a90e5a755eda412a2f633983f3dedcf
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-02-12 21:10:07 -0500

    Geocode universities

---
+++ geocode.r
@@ -0,0 +1,7 @@
+# Geocode the universities
+library(ggmap)
+geocoded <- geocode(university_count$university)
+university_count$lon <- geocoded$lon
+university_count$lat <- geocoded$lat
+university_count <- filter(university_count, !is.na(lon))
+write.csv(university_count, "location/universities-geocoded-r.csv")
